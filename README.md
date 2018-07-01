eed43bf42bde42bba0d46e7819ca99b8fa9524ae
Jakob Dietrich <jakibaki@live.com>
2018-07-01 12:27:58 +0200

    Playing mp3s now :)

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is (hopefully) going to be an background-audio-player (as a custom sysmodule) for the Nintendo Switch.
 
-This is partially based on nx-dreport.
+It is partially based on nx-dreport and uses some code from ctrmus.
--- source/main.c
+++ source/main.c
@@ -3,62 +3,42 @@
 #include <stdlib.h>
 #include <malloc.h>
 #include <math.h>
+#include <mpg123.h>
 
 #include <switch.h>
-
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_mixer.h>
 
 #define ERPT_SAVE_ID 0x80000000000000D1
 #define TITLE_ID 0x4200000000000000
 #define HEAP_SIZE 0x000040000
-#define M_PI 3.14159265358979323846
-
-
-#define SAMPLERATE 48000
-#define CHANNELCOUNT 2
-#define FRAMERATE (1000 / 30)
-#define SAMPLECOUNT (SAMPLERATE / FRAMERATE)
-#define BYTESPERSAMPLE 2
-
-void fill_audio_buffer(void* audio_buffer, size_t offset, size_t size, int frequency) {
-    if (audio_buffer == NULL) return;
-    
-    u32* dest = (u32*) audio_buffer;
-    for (size_t i = 0; i < size; i++) {
-        // This is a simple sine wave, with a frequency of `frequency` Hz, and an amplitude 30% of maximum.
-        s16 sample = 0.3 * 0x7FFF * sin(frequency * (2 * M_PI) * (offset + i) / SAMPLERATE);
-
-        // Stereo samples are interleaved: left and right channels.
-        dest[i] = (sample << 16) | (sample & 0xffff);
-    }
-}
 
 
 // we aren't an applet
 u32 __nx_applet_type = AppletType_None;
 
 // setup a fake heap (we don't need the heap anyway)
-char   fake_heap[HEAP_SIZE];
+char fake_heap[HEAP_SIZE];
 
-void fatalLater(Result err) {
+void fatalLater(Result err)
+{
     Handle srv;
 
-    while (R_FAILED(smGetServiceOriginal(&srv, smEncodeName("fatal:u")))) {
+    while (R_FAILED(smGetServiceOriginal(&srv, smEncodeName("fatal:u"))))
+    {
         // wait one sec and retry
         svcSleepThread(1000000000L);
     }
 
-    // fatal is here time, fatal like a boss    
+    // fatal is here time, fatal like a boss
     IpcCommand c;
     ipcInitialize(&c);
     ipcSendPid(&c);
-    struct {
+    struct
+    {
         u64 magic;
         u64 cmd_id;
         u64 result;
         u64 unknown;
-    } *raw;
+    } * raw;
 
     raw = ipcPrepareHeader(&c, sizeof(*raw));
 
@@ -72,18 +52,20 @@ void fatalLater(Result err) {
 }
 
 // we override libnx internals to do a minimal init
-void __libnx_initheap(void) {
-    extern char* fake_heap_start;
-    extern char* fake_heap_end;
+void __libnx_initheap(void)
+{
+    extern char *fake_heap_start;
+    extern char *fake_heap_end;
 
     // setup newlib fake heap
     fake_heap_start = fake_heap;
-    fake_heap_end   = fake_heap + HEAP_SIZE;
+    fake_heap_end = fake_heap + HEAP_SIZE;
 }
 
-void __appInit(void) {
+void __appInit(void)
+{
     Result rc;
-    svcSleepThread(5000000000L);
+    svcSleepThread(10000000000L);
 
     rc = smInitialize();
     if (R_FAILED(rc))
@@ -94,10 +76,10 @@ void __appInit(void) {
     rc = fsdevMountSdmc();
     if (R_FAILED(rc))
         fatalLater(rc);
-
 }
 
-void __appExit(void) {
+void __appExit(void)
+{
     fsdevUnmountAll();
     fsExit();
     smExit();
@@ -105,86 +87,159 @@ void __appExit(void) {
 
 
 
-int main(int argc, char **argv) {
+
+#define BUF_COUNT 2
+
+static size_t			buffSize;
+static mpg123_handle	*mh = NULL;
+static uint32_t			rate;
+static uint8_t			channels;
+
+AudioOutBuffer*         audout_released_buf;
+
+AudioOutBuffer          audiobuf[BUF_COUNT];
+u8*                     buffData[BUF_COUNT];
+int                     curBuf = 0;
+#define swapbuf (curBuf = (curBuf+1)%(BUF_COUNT))
+
+
+
+int initMp3(const char* file)
+{
+	int err = 0;
+	int encoding = 0;
+
+	if((err = mpg123_init()) != MPG123_OK)
+		return err;
+
+
+	if((mh = mpg123_new(NULL, &err)) == NULL)
+	{
+		printf("Error: %s\n", mpg123_plain_strerror(err));
+		return err;
+	}
+
+
+
+	if(mpg123_open(mh, file) != MPG123_OK ||
+			mpg123_getformat(mh, (long *) &rate, (int *) &channels, &encoding) != MPG123_OK)
+	{
+		printf("Trouble with mpg123: %s\n", mpg123_strerror(mh));
+		return -1;
+	}
+
+	/*
+	 * Ensure that this output format will not change (it might, when we allow
+	 * it).
+	 */
+	mpg123_format_none(mh);
+	mpg123_format(mh, rate, channels, encoding);
+
+	/*
+	 * Buffer could be almost any size here, mpg123_outblock() is just some
+	 * recommendation. The size should be a multiple of the PCM frame size.
+	 */
+	buffSize = mpg123_outblock(mh) * 16;
+
+	return 0;
+}
+
+/**
+ * Get sampling rate of MP3 file.
+ *
+ * \return	Sampling rate.
+ */
+uint32_t rateMp3(void)
+{
+	return rate;
+}
+
+/**
+ * Get number of channels of MP3 file.
+ *
+ * \return	Number of channels for opened file.
+ */
+uint8_t channelMp3(void)
+{
+	return channels;
+}
+
+/**
+ * Decode part of open MP3 file.
+ *
+ * \param buffer	Decoded output.
+ * \return			Samples read for each channel.
+ */
+uint64_t decodeMp3(void* buffer)
+{
+	size_t done = 0;
+	mpg123_read(mh, buffer, buffSize, &done);
+	return done / (sizeof(int16_t));
+}
+
+/**
+ * Free MP3 decoder.
+ */
+void exitMp3(void)
+{
+	mpg123_close(mh);
+	mpg123_delete(mh);
+	mpg123_exit();
+}
+
+int fillBuf() {
+    decodeMp3(buffData[curBuf]);
+    audiobuf[curBuf].next = 0;
+    audiobuf[curBuf].buffer = buffData[curBuf];
+    audiobuf[curBuf].buffer_size = buffSize;
+    audiobuf[curBuf].data_size = buffSize;
+    audiobuf[curBuf].data_offset = 0;
+    audoutAppendAudioOutBuffer(&audiobuf[curBuf]);
+    swapbuf;
+    return 0;
+}
+
+
+
+
+int main(int argc, char **argv)
+{
     (void)argc;
     (void)argv;
-    FILE* f = fopen("/log", "w");
-    if(f == NULL) {
-        fatalSimple(12);
-    }
+    FILE *f = fopen("/log", "w");
 
     stdout = f;
     stderr = f;
 
     Result rc = 0;
 
-    printf("Init successful... Waiting for 15 seconds now for audio...\n");
+    audoutInitialize();
+    audoutStartAudioOut();
+    initMp3("/test.mp3");
 
-    AudioOutBuffer audout_buf;
-    AudioOutBuffer *audout_released_buf;
+    u32 released_count = 0;
+
+    for(int curBuf = 0; curBuf < BUF_COUNT; curBuf++)
+        buffData[curBuf] = memalign(0x1000, buffSize);
+
     
-    // Make sure the sample buffer size is aligned to 0x1000 bytes.
-    u32 data_size = (SAMPLECOUNT * CHANNELCOUNT * BYTESPERSAMPLE);
-    u32 buffer_size = (data_size + 0xfff) & ~0xfff;
+    for(int curBuf = 0; curBuf < BUF_COUNT/2; curBuf++)
+        fillBuf();
     
-    // Allocate the buffer.
-    u8* out_buf_data = memalign(0x1000, buffer_size);
-    
-    // Ensure buffers were properly allocated.
-    if (out_buf_data == NULL)
+    int i = 0;
+    while(1)
     {
-        rc = MAKERESULT(Module_Libnx, LibnxError_OutOfMemory);
-        printf("Failed to allocate sample data buffers\n");
-        fclose(f);
-        fatalSimple(rc);
-    }
+        for(int curBuf = 0; curBuf < BUF_COUNT/2; curBuf++)
+            fillBuf();
     
-    if (R_SUCCEEDED(rc))
-        memset(out_buf_data, 0, buffer_size);
-    
+        for(int curBuf = 0; curBuf < BUF_COUNT/2; curBuf++)
+            audoutWaitPlayFinish(&audout_released_buf, &released_count, U64_MAX);
 
-    rc = audoutInitialize();
-    printf("audoutInitialize() returned 0x%x\n", rc);
-    if(R_FAILED(rc)) {
-        fclose(f);
-        fatalSimple(rc);
     }
-    printf("Sample rate: 0x%x\n", audoutGetSampleRate());
-    printf("Channel count: 0x%x\n", audoutGetChannelCount());
-    printf("PCM format: 0x%x\n", audoutGetPcmFormat());
-    printf("Device state: 0x%x\n", audoutGetDeviceState());
-    
-    // Start audio playback.
-    rc = audoutStartAudioOut();
-    printf("audoutStartAudioOut() returned 0x%x\n", rc);
-    if(R_FAILED(rc)) {
-        fclose(f);
-        fatalSimple(rc);
-    }
-
-    for(;;) {
-        fill_audio_buffer(out_buf_data, 0, data_size, 220);
-        audout_buf.next = NULL;
-        audout_buf.buffer = out_buf_data;
-        audout_buf.buffer_size = buffer_size;
-        audout_buf.data_size = data_size;
-        audout_buf.data_offset = 0;
-        
-        // Prepare pointer for the released buffer.
-        audout_released_buf = NULL;
-        
-        // Play the buffer.
-        rc = audoutPlayBuffer(&audout_buf, &audout_released_buf);
-        
-        if (R_FAILED(rc)) {
-            printf("audoutPlayBuffer() returned 0x%x\n", rc);
-            fclose(f);
-            fatalSimple(rc);
-        }
-        svcSleepThread(1000000000L);
-    }
+    printf("Yo motherfucker, guess who's here.\n");
 
 
     fclose(f);
+
     return 0;
 }
