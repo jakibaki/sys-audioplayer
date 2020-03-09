6243d6480ada96096f3f3fce94db692a56025f60
Mithos23 <tfritz420@googlemail.com>
2020-03-09 22:59:53 +0100

    fatalSimple to fatalThrow

--- source/main.c
+++ source/main.c
@@ -36,21 +36,21 @@ void __attribute__((weak)) __appInit(void)
 
     rc = smInitialize();
     if (R_FAILED(rc))
-        fatalSimple(MAKERESULT(Module_Libnx, LibnxError_InitFail_SM));
+        fatalThrow(MAKERESULT(Module_Libnx, LibnxError_InitFail_SM));
         
     rc = timeInitialize();
     if (R_FAILED(rc))
-        fatalSimple(MAKERESULT(Module_Libnx, LibnxError_InitFail_Time));
+        fatalThrow(MAKERESULT(Module_Libnx, LibnxError_InitFail_Time));
 
     __libnx_init_time();
 
     rc = hidInitialize();
     if (R_FAILED(rc))
-        fatalSimple(MAKERESULT(Module_Libnx, LibnxError_InitFail_HID));
+        fatalThrow(MAKERESULT(Module_Libnx, LibnxError_InitFail_HID));
 
     rc = fsInitialize();
     if (R_FAILED(rc))
-        fatalSimple(MAKERESULT(Module_Libnx, LibnxError_InitFail_FS));
+        fatalThrow(MAKERESULT(Module_Libnx, LibnxError_InitFail_FS));
 
     fsdevMountSdmc();
 }
